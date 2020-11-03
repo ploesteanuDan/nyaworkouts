8a94e0b3c01b072cd61f002ba7dd7b8895c8a26d
ploesteanuDan <ploesteanudan.99@gmail.com>
2020-11-03 21:52:28 +0200

    ach

--- components/AchievementsScreen.js
+++ components/AchievementsScreen.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   StyleSheet,
   Text,
@@ -7,10 +7,10 @@ import {
   Dimensions,
   ScrollView,
 } from "react-native";
+import { Link } from "react-router-native";
 import * as firebase from "firebase";
 import { FlatList, TouchableOpacity } from "react-native-gesture-handler";
-import { useSpring, animated, config } from "react-spring";
-
+import { Spring } from "react-spring/renderprops";
 const achievements = [
   {
     key: "0",
@@ -32,123 +32,128 @@ const achievements = [
   },
 ];
 
+
+
+
 export default function AchievementsScreen() {
-  const loadAnim = useSpring({
-    left: 0,
-    opacity: 1,
-    from: { left: -30, opacity: 0 },
-  });
-  const AnimatedView = animated(View);
+  const dummy = {
+    key: "2",
+    name: "",
+    photo: "https://picsum.photos/452",
+    reason: "Achieved for completing 3 leg based sessions.",
+  }
+  const [item, setItem] = useState(dummy)
   return (
-    <AnimatedView style={loadAnim}>
-      <View style={styles.AvhievementsScreenContainer}>
+    <Spring
+    from={{
+      opacity: 0,
+      top: "7%",
+      left: "2.5%",
+      height: "95%",
+      left: "-5%",
+    }}
+    to={{ opacity: 1, top: "7%", left: "0%", height: "95%" }}
+  >
+    {(mainProps) => (
+      <View style={mainProps}>
         <Text style={styles.title}>Achievements</Text>
+        <Spring
+                  from={{ 
+                    opacity: 0,
+                    
+                   }}
+                  to={{ 
+                    position: "absolute",
+                    opacity: item.name != "" ? 1 : 0,
+                    width: item.name != "" ? Dimensions.get("window").width * 0.45 : 0,
+                    height: item.name != "" ? Dimensions.get("window").width * 0.45 : 0,
+                    width: "100%",
+                    height: "110%",
+                    zIndex: item.name != "" ? 9999 : 0,
+                    backgroundColor: "black",  
+                    top: item.name != "" ? "-10%" : "-200%"                
+                    }}>
+                  {popProps => 
+                  <View style={popProps}>
+                  <Link 
+                    onPress={()=>{setItem(dummy)}}
+                  >
+                  <View>
+                  <Image
+                  style={styles.popupImage}
+                  source={{uri: item.photo}}
+                  />
+                  <Text style={styles.achievementName}>{item.name}</Text>
+                  <Text style={styles.achievementReason}>{item.reason}</Text>
+                  </View>
+                </Link>
+                </View>
+                }
+                </Spring>
         <ScrollView>
           <FlatList
+            numColumns={3}
             data={achievements}
-            renderItem={({ item }) => (
-              <View style={styles.achievementCards}>
-                <Image
-                  style={styles.achievementCardBg}
-                  source={{ uri: item.photo }}
-                />
-
-                <Text style={styles.achievementCardName}>{item.name}</Text>
-
-                <Text style={styles.achievementCardReason}>{item.reason}</Text>
+            renderItem={({item})=>(
+              <View style={styles.achievementPackets}>
+                <Link
+                  onPress={()=>{setItem(item)}}
+                >
+                  <Image
+                  style={styles.achievementImage}
+                  source={{uri: item.photo}}
+                  />
+                </Link>              
               </View>
             )}
-          />
-          <Text style={styles.noMoreCards}>
-            These are all your achievements. Work hard and gain more!
-          </Text>
-          <TouchableOpacity>
-            <Text style={styles.supportUsButton}>Support us</Text>
-          </TouchableOpacity>
+          >
+
+          </FlatList>
         </ScrollView>
-      </View>
-    </AnimatedView>
-  );
+      </View>)}
+    </Spring>
+  )
 }
 
 const styles = StyleSheet.create({
-  AvhievementsScreenContainer: {
-    top: "7%",
-    height: "95%",
-  },
   title: {
     fontSize: 45,
     color: "#D2D2D2",
     margin: 20,
     fontFamily: "Poppins_800ExtraBold_Italic",
   },
-  infoContainer: {
-    backgroundColor: "#191919",
-    width: "90%",
-    height: 300,
-    alignSelf: "center",
-    borderRadius: 12,
-  },
-  supportUsButton: {
-    alignSelf: "center",
-    width: 140,
-    height: 50,
-    backgroundColor: "#cf8061",
-    color: "#D2D2D2",
-    borderRadius: 12,
-    textAlign: "center",
-    lineHeight: 65,
-    fontSize: 25,
-    fontFamily: "Poppins_600SemiBold",
-    marginTop: 30,
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 6,
-    },
-    shadowOpacity: 0.39,
-    shadowRadius: 8.3,
-
-    elevation: 13,
-    marginBottom: 150,
-  },
-  achievementCards: {
+  achievementPackets: {
     backgroundColor: "#1D1D1D",
-    width: Dimensions.get("window").width * 0.95,
-    height: Dimensions.get("window").width * 0.95,
-    marginBottom: 10,
-    marginTop: 10,
+    width: Dimensions.get("window").width * 0.3,
+    height: Dimensions.get("window").width * 0.3,
+    margin: Dimensions.get("window").width * 0.016,
     borderRadius: 12,
     overflow: "hidden",
-    alignSelf: "center",
   },
-  achievementCardName: {
-    color: "#D2D2D2",
-    fontSize: 40,
-    position: "absolute",
-    bottom: "30%",
-    left: "5%",
-
-    fontFamily: "Poppins_800ExtraBold_Italic",
-  },
-  achievementCardReason: {
-    color: "#D2D2D2",
-    fontSize: 15,
-    position: "absolute",
-    bottom: "25%",
-    left: "5%",
-    fontFamily: "Poppins_400Regular",
-  },
-  achievementCardBg: {
+  achievementImage: {
     width: "100%",
-    height: "100%",
+    height: "100%",   
   },
-  noMoreCards: {
-    fontSize: 17,
-    marginBottom: 150,
+  popupImage: {
+    width: Dimensions.get("window").width * 0.9,
+    height: Dimensions.get("window").width * 0.9,
+    margin: Dimensions.get("window").width * 0.05,
+    borderRadius: 12,
+    top: "20%"
+  },
+  achievementName: {
+    top: "20%",
+    color: "#D2D2D2",
+    fontSize: 50,
+    left: "5%",
+    fontFamily: "Poppins_800ExtraBold_Italic",
+    lineHeight: 60,
+  },
+  achievementReason: {
+    top: "20%",
+    fontSize: 20,
     color: "#D2D2D2",
     fontFamily: "Poppins_400Regular",
-    width: "90%",
-    alignSelf: "center",
-  },
-});
+    left: "5%",
+  }
+})
